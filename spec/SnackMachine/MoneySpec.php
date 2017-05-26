49363b5b7d55b62bd0431fe57b7cc492ce6d8d0f
Anael CHARDAN <anael.chardan@gmail.com>
2017-05-26 15:33:17 +0200

    Test Spec

--- spec/SnackMachine/MoneySpec.php
+++ spec/SnackMachine/MoneySpec.php
@@ -4,7 +4,6 @@ namespace spec\SnackMachine;
 
 use SnackMachine\Money;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class MoneySpec extends ObjectBehavior
 {
@@ -18,18 +17,25 @@ class MoneySpec extends ObjectBehavior
         $this->shouldBeAnInstanceOf(Money::class);
     }
 
-    function it_shoud_be_equals_to(Money $money)
+    function it_shoud_be_equals_to(Money $money1, Money $result)
     {
-        $money1 = new Money(1, 2, 3, 4, 5, 6);
-        $money2 = new Money(2, 4, 6, 8, 10, 12);
+//        $money1 = new Money(1, 2, 3, 4, 5, 6);
+//        $money2 = new Money(2, 4, 6, 8, 10, 12);
 
-        $money->getOneCentCount()->willReturn(2);
-        $money->getTenCentCount()->willReturn(4);
-        $money->getQuarterCount()->willReturn(6);
-        $money->getOneDollarCount()->willReturn(8);
-        $money->getFiveDollarCount()->willReturn(10);
-        $money->getTwentyDollarCount()->willReturn(12);
+        $money1->getOneCentCount()->willReturn(1);
+        $money1->getTenCentCount()->willReturn(2);
+        $money1->getQuarterCount()->willReturn(3);
+        $money1->getOneDollarCount()->willReturn(4);
+        $money1->getFiveDollarCount()->willReturn(5);
+        $money1->getTwentyDollarCount()->willReturn(6);
 
-        $this->addMoney($money1)->shouldReturn();
+        $result->getOneCentCount()->willReturn(2);
+        $result->getTenCentCount()->willReturn(4);
+        $result->getQuarterCount()->willReturn(6);
+        $result->getOneDollarCount()->willReturn(8);
+        $result->getFiveDollarCount()->willReturn(10);
+        $result->getTwentyDollarCount()->willReturn(12);
+
+        $this->addMoney($money1)->shouldReturn($result);
     }
 }
